7bc4da2ec3a80802a67ff998ba20b71a38ed6bf3
nicxes <nicxesh@gmail.com>
2023-12-15 01:31:31 -0300

    fix(income): Added DB Class

--- app/Models/Income.php
+++ app/Models/Income.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class Income extends Model
 {
