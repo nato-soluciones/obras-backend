1eef5b4476aa0c92e4bc0919933052fe774d0590
nicxes <nicxesh@gmail.com>
2023-12-15 01:37:20 -0300

    fix(income): Error table name

--- database/migrations/2023_12_15_033315_create_income_table.php
+++ database/migrations/2023_12_15_033315_create_income_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('income', function (Blueprint $table) {
+        Schema::create('incomes', function (Blueprint $table) {
             $table->id();
             $table->string('receipt_number')->unique();
             $table->date('date');
