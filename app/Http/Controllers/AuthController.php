8b70e46fc7d5f3862386554d2f2f7cdd0e30656f
nicxes <nicxesh@gmail.com>
2024-01-08 16:50:52 -0300

    fix(auth): Added endpoint on api for authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'The provided credentials are incorrect.'
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\BudgetController;
 use App\Http\Controllers\ObraController;
@@ -20,6 +21,12 @@ use App\Http\Controllers\ContractorController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Authentications endpoint
+Route::prefix('auth')->controller(AuthController::class)->group(function() {
+    Route::post('/login', 'login');
+});
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
