2fa474e453609b464b113d95dda66ec205aba3a4
nicxes <nicxesh@gmail.com>
2024-04-03 19:55:37 -0300

    fix(contractors): Added seeder industries

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(UserSeeder::class);
+        $this->call(ContractorIndustriesSeeder::class);
         $this->call(ToolCategorySeeder::class);
         $this->call(ManufacturerCategories::class);
     }
