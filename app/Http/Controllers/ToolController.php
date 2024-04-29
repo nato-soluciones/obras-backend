0c974c4abaf0b92afc70718aea209d08cfce442f
Pablo-PC <pcassettai@gmail.com>
2024-04-29 10:58:16 -0300

    (tools) added order locations by date

--- app/Http/Controllers/ToolController.php
+++ app/Http/Controllers/ToolController.php
@@ -20,7 +20,7 @@ class ToolController extends Controller
      */
     public function index(): Response
     {
-        $tools = Tool::with('category', 'last_location')->get();
+        $tools = Tool::with(['category', 'last_location'])->get();
         return response($tools, 200);
     }
 
@@ -58,7 +58,9 @@ class ToolController extends Controller
      */
     public function show(int $id): Response
     {
-        $tool = Tool::with('locations')->find($id);
+        $tool = Tool::with(['locations' => function($query){
+            $query->orderBy('date', 'asc');
+        }])->find($id);
         return response($tool, 200);
     }
 
