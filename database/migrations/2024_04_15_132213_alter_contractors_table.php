ee6fb591f34276b78105c663fc2218c77fe3058f
nicxes <nicxesh@gmail.com>
2024-04-29 19:13:28 -0300

    fix(contractors): Person type default value individual

--- database/migrations/2024_04_15_132213_alter_contractors_table.php
+++ database/migrations/2024_04_15_132213_alter_contractors_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('contractors', function (Blueprint $table) {
-            $table->string('person_type');
+            $table->string('person_type')->default('individual');
         });
     }
 
