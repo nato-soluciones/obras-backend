0b2910f8bb0ac44c3159bd2a612327aff5422c30
nicxes <nicxesh@gmail.com>
2024-05-06 10:03:06 -0300

    fix(mails): Displaying correctly logo + url link

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,7 @@
-@props(['url'])
 <tr>
   <td class="header">
-    <a href="{{ $url }}" style="display: inline-block;">
-      @if (trim($slot) === 'Laravel')
-        <img src="{{ env('APP_URL') }}/images/nato.png" class="logo" alt="Nato Inc.">
-      @else
-        {{ $slot }}
-      @endif
+    <a href="{{ env('FRONTEND_URL') }}" style="display: inline-block;">
+      <img src="{{ env('APP_URL') }}/images/logo.png" class="logo" alt="{{ $slot }}">
     </a>
   </td>
 </tr>
