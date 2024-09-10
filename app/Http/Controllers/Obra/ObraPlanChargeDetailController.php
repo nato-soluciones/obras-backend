65ec8bf8016d5d70e98f31d151c327f8ce1700e1
GJRC23 <gastuconsoli8@gmail.com>
2024-09-10 16:01:40 -0300

    paginacionde 5 a 10

--- app/Http/Controllers/Obra/ObraPlanChargeDetailController.php
+++ app/Http/Controllers/Obra/ObraPlanChargeDetailController.php
@@ -22,7 +22,7 @@ class ObraPlanChargeDetailController extends Controller
 
     public function index(Request $request, int $obraId)
     {
-        $per_page = 5;
+        $per_page = 10;
         $payment_status = $request->input('paymentStatus');
 
         $details = ObraPlanChargeDetail::whereHas('planCharge', function ($query) use ($obraId) {
