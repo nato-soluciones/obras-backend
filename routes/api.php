5cf878a9acb72b6ab507a60f3aeb94e63119ac51
tnapolitano <tnapolitano@mobydigital.com>
2024-12-11 09:05:36 -0300

    Adding Store route file to general api route file

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,7 @@ include_once __DIR__ . '/api/obras.php';
 include_once __DIR__ . '/api/fleets.php';
 include_once __DIR__ . '/api/permissions.php';
 include_once __DIR__ . '/api/companies.php';
+include_once __DIR__ . '/api/stores.php';
 
 Route::prefix('initial_settings')->middleware('auth:sanctum')->controller(InitialSettingController::class)->group(function () {
     Route::get('/', 'index');
